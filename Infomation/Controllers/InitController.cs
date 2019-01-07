da7728c7e1309dee6fbb94d474e520a3f83a1bf4
chr <intothesun@163.com>
2019-01-07 19:06:48 +0800

    登陆页面提交

--- Infomation/Controllers/InitController.cs
+++ Infomation/Controllers/InitController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Infomation.Controllers
@@ -17,6 +18,22 @@ namespace Infomation.Controllers
         public IActionResult Init()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(IFormCollection form)
+        {
+            //获取参数
+            string userName = form["userName"];
+            string passWord = form["password"];
+            string validateCode = form["validateCode"];
+
+            //调用服务
+            
+
+            string msg="";
+
+            return Content(msg);
         }
 
         /// <summary>
