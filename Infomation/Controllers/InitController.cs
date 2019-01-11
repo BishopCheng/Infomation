ab11b8a96a08b01a712ed34867c8f1db06ee321d
chr <intothesun@163.com>
2019-01-11 19:20:52 +0800

    平台修改

--- Infomation/Controllers/InitController.cs
+++ Infomation/Controllers/InitController.cs
@@ -19,38 +19,9 @@ namespace Infomation.Controllers
 
         public IActionResult Init()
         {
-            //加载页面时生成验证码，生成后添加入缓存，然后显示到页面上
-            string webapiurl = "http://localhost:51385/LoginService/login";
-             
-            var httpResponse = new HttpResponseMessage();
-            using (var httpClient  = new HttpClient())
-            {
-                 
-
-            }
-                return View();
+            return View();
         }
-
-        public IActionResult GetVilidateCode()
-        {
-            async void Get()
-            {
-                //调用WEBAPI
-                string webApiUrl = "http://localhost:51385/LoginService/GetValidateCode";
-                //创建HttpClient
-                var handler = new HttpClientHandler() { AutomaticDecompression = System.Net.DecompressionMethods.GZip };
-                using (var http = new HttpClient(handler))
-                {
-                    //await异步等待回应
-                    var response = await http.GetAsync(webApiUrl);
-                    //确保HTTP成功状态值
-                    response.EnsureSuccessStatusCode();
-                    var File =  response.Content;
-                }
-            }
-            return File;
-        }
-
+       
         [HttpPost]
         public ActionResult Login(IFormCollection form)
         {
